32bbd6bf90b4d145a3cc77d89453a27571566022
Calysin <chenirene1006@gmail.com>
2024-05-14 15:17:10 +0200

    fin de la partie 1

---
+++ cdataframe.h
@@ -0,0 +1,12 @@
+#include "column.h"
+
+#ifndef PROJET_CDATAFRAME_CDATAFRAME_H
+#define PROJET_CDATAFRAME_CDATAFRAME_H
+
+int CreationCDataFrameVide(int NombreCases);
+
+typedef struct {
+    COLUMN colonnes;
+} CDataframe;
+
+#endif //PROJET_CDATAFRAME_CDATAFRAME_H
